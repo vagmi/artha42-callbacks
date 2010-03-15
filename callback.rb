188c1eeb284a09dd285875023400d13572f6239c
Vagmi Mudumbai <vagmi@deepthought.(none)>
2010-03-15 19:06:45 +0530

    add post-recieve hook from github

--- callback.rb
+++ callback.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'sinatra'
-
+require 'json'
 get '/' do
   "Hello world"
 end
+
+post '/gh-post-recieve/' do
+  push = JSON.parse(params[:payload])
+  "I got some JSON: #{push.inspect}"
+end 
