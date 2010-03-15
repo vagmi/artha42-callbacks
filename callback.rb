7eabd5ddbf574f8a6a24ac9c580391254414b7ed
Vagmi Mudumbai <vagmi@deepthought.(none)>
2010-03-15 23:41:26 +0530

    duh! the password is wrong. still needs cleanup

--- callback.rb
+++ callback.rb
@@ -3,32 +3,56 @@ require 'sinatra'
 require 'cgi'
 require 'json'
 require 'httparty'
+use_in_file_templates!
+
+get '/test' do 
+  @content=File.read("/home/vagmi/work/response.json")
+  erb :index
+end
 
 class StatusNet
   include HTTParty
   base_uri "artha42.status.net/api"
-  basic_auth "artha42","42athra"
+  #TODO: Read this from the database
+  basic_auth "artha42","24ahtra"
   def self.update(message)
     self.post('/statuses/update.json',:query=>{:status=>message})
   end
 end
 
-post '/gh-post-recieve/' do
+post '/gh-post-receive/' do
   payload = JSON.parse(params[:payload])
   repo=payload["repository"]
   message=""
   if(payload["commits"].count>1)
     before=payload["before"]
     after=payload["after"]
-    old_url=CGI.escape("#{repo["url"]}/compare/#{before}...#{after}")
+    old_url="#{repo["url"]}/compare/#{before}...#{after}"
     short_url=HTTParty.get("http://is.gd/api.php",:query=>{:longurl=>old_url})
-    message="recieved a bunch of commits to #{repo["name"]} via #github #{short_url}"
+    message="recieved a bunch of commits to ##{repo["name"]} via #github #{short_url}"
   else
     commit=payload["commits"][0]
-    old_url=CGI.escape(commit["url"])
+    old_url=commit["url"]
     short_url=HTTParty.get("http://is.gd/api.php",:query=>{:longurl=>old_url})
     author=commit["author"]["name"]
-    message="#{author} pushed a change to #{repo["name"]} via #github #{short_url}"
+    message="#{author} pushed a change to ##{repo["name"]} via #github #{short_url}"
   end
   StatusNet.update(message)
 end
+
+__END__
+
+@@index
+<html>
+  <head>
+    <title>Test json post</title>
+  </head>
+  <body>
+  <form action="/gh-post-receive/" method="post">
+    <textarea name="payload">
+    <%=@content%>
+    </textarea>
+    <input type="submit" value="post"/>
+  </form>
+  </body>
+</html>
